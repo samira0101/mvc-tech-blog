7e17d336d3cf2d3f2c5f893b259ad585924325d8
Allure <samira-h@hotmail.co.uk>
2022-04-02 23:16:31 +0100

    render the posts function

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -33,3 +33,19 @@ router.get('/', (req, res) => {
             }
         ]
     })
+    // render the posts
+    .then(dbPostData => {
+      // Make an array for the posts and use the get method to remove any unnecessary sequelize object data.
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      // pass the posts into the homepage template
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    // if server error, return the error
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
