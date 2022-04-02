cb2d74ad789ba9b3f7147d652875d7f97c7989a4
Allure <samira-h@hotmail.co.uk>
2022-04-02 23:11:56 +0100

    route to edit the logged in users

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -90,3 +90,33 @@ router.get('/edit/:id', withAuth, (req, res) => {
       res.status(500).json(err);
     });
 });
+
+// A route to edit the logged in user
+router.get('/edituser', withAuth, (req, res) => {
+  // To acquire a single user depending on parameters, access the User model and use the findOne() function.
+  User.findOne({
+    // when the data is sent back, exclude the password property
+    attributes: { exclude: ['password'] },
+    where: {
+      // use id as the parameter for the request
+      id: req.session.user_id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        // if no user is found, return an error
+        res.status(404).json({ message: 'There are no users with this id found.' });
+        return;
+      }
+      // otherwise, return the data for the requested user
+      const user = dbUserData.get({ plain: true });
+      res.render('edit-user', {user, loggedIn: true});
+    })
+    .catch(err => {
+      // if there is a server error, return that error
+      console.log(err);
+      res.status(500).json(err);
+    })
+  });
+
+module.exports = router;
