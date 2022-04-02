5644ded948497f6bb4deddf9e2d070facffb6325
Allure <samira-h@hotmail.co.uk>
2022-04-02 23:10:52 +0100

    functions to edit post

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -47,3 +47,46 @@ router.get('/', withAuth, (req, res) => {
       });
   });
 
+// A route to edit a post
+router.get('/edit/:id', withAuth, (req, res) => {
+  // All of the users posts are obtained from the database
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'post_text',
+      'title',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      // Return an error if no post with that id exists.
+      if (!dbPostData) {
+        res.status(404).json({ message: 'This id was not found in any posts.' });
+        return;
+      }
+      // serialize data before passing to template
+      const post = dbPostData.get({ plain: true });
+      res.render('edit-post', { post, loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
